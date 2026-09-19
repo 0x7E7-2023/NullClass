5d6e96c3efabad2baecd8dd9bf1a881bff38ffd7
0x7E7-2023 <0x7E7-2023@users.noreply.github.com>
2026-09-19 23:05:36 +0800

    chore(release): 0.9.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.nullclass.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 27
-        versionName = "0.9.8"
+        versionCode = 28
+        versionName = "0.9.9"
     }
 
     /**
